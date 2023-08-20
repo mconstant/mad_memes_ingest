1ca85ad58f93047cc64ce0b8a04f8b1b7f078d68
Anonymous <satoshin@gmx.net>
2023-08-20 14:13:45 -0400

    channel history

--- mad_memes_ingest.rb
+++ mad_memes_ingest.rb
@@ -2,8 +2,20 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new token: 'MTE0Mjg3Mzk0Nzk0MDA3MzU3Mg.Gv0nZe.x6iLXDFjUrzSyjvkFdYLGFMcp_eqe4f5JHN444'
 
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
+messages_array = []
+bot.ready do |event|
+  # Get a channel by its ID (replace 'CHANNEL_ID' with your channel's ID)
+  channel = bot.channel('837474873818349579')
+
+  # Get the last 100 messages from the channel
+  messages = channel.history(100)
+
+  # Store the text of each message in the array
+  messages.each do |message|
+    messages_array << message.content
+  end
 end
+
+print messages_array
 
 bot.run
