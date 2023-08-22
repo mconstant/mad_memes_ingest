3a7d008326c1f9ba624f5294f28dad543dffcf30
mconstant <85532172+mconstant@users.noreply.github.com>
2023-08-22 19:45:46 +0000

    try spreadsheet key

--- mad_memes_ingest.rb
+++ mad_memes_ingest.rb
@@ -59,7 +59,7 @@ end
 # Use the session to interact with Google Sheets API
 # For example, you can access a spreadsheet
 puts "Loading Spreadsheet"
-spreadsheet = session.spreadsheet_by_title("mad_memes_ingest_v0.0.1")
+spreadsheet = session.spreadsheet_by_key("1o5Jvo-wSyNiCAzIDGFFqciGQqZ-2bF9is1WfCctdicc")
 puts "Loading Worksheet"
 worksheet = spreadsheet.worksheets[0]
 puts worksheet.title
