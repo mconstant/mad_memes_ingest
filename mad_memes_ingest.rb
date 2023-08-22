a3f77130b9acd12d247639a67b40f351bf65ddf0
mconstant <85532172+mconstant@users.noreply.github.com>
2023-08-22 17:20:19 +0000

    more debugging

--- mad_memes_ingest.rb
+++ mad_memes_ingest.rb
@@ -1,28 +1,45 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
+begin
+  puts "Instantiating Bot"
+  bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
 
-messages_array = []
-bot.ready do |event|
-  bot.on do |interaction|
-    interaction.member.fetch
+  puts "Declaring empty messages_array"
+  messages_array = []
 
-    # Get a channel by its ID (replace 'CHANNEL_ID' with your channel's ID)
-    channel = bot.channel('837474873818349579')
+  puts "Entering bot.ready block"
+  bot.ready do |event|
+    puts "In bot.ready block"
+    puts "Entering bot.on block"
+    bot.on do |interaction|
+      puts "In bot.on block"
+      puts "Getting Bot Member"
+      interaction.member.fetch
 
-    # Get the last 100 messages from the channel
-    messages = channel.history(100)
+      # Get a channel by its ID (replace 'CHANNEL_ID' with your channel's ID)
+      puts "Loading Memes Channel..."
+      channel = bot.channel('837474873818349579')
 
-    # Store the text of each message in the array
-    messages.each do |message|
-      messages_array << message.content
+      puts "Getting message content"
+      # Get the last 100 messages from the channel
+      messages = channel.history(100)
+
+      puts "Loading Message content into array"
+      # Store the text of each message in the array
+      messages.each do |message|
+        messages_array << message.content
+      end
+
+      puts "printing out message content"
+      puts "printing messages array:"
+      print messages_array
     end
-
-    puts "printing messages array:"
-    print messages_array
   end
-end
 
-bot.run
+  bot.run
+rescue Exception => e
+  puts "had to rescue!"
+  puts "Exception:\n#{e.backtrace.join("\n")}"
+end
 
 
