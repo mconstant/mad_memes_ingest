71ccb633a1835d75bb044219cd4926cbd1a07e20
Anonymous <satoshin@gmx.net>
2023-09-08 23:26:25 -0400

    room for error

--- mad_memes_ingest.rb
+++ mad_memes_ingest.rb
@@ -120,7 +120,7 @@ sample_rarities.each do |rarity|
   puts "Rarity #{rarity} belongs to #{category} category."
 end
 
-rarity_bag = (1..memes_count).to_a.shuffle
+rarity_bag = (1..(memes_count+1000)).to_a.shuffle
 
 messages_array.each_with_index do |row, idx|
   rarity = rarity_bag.pop
