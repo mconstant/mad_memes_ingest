fb06626590fad3e9eee2984f13e2b7f2dfb913ab
Anonymous <satoshin@gmx.net>
2023-09-11 11:50:57 -0400

    hardcoded rarity curve fix 2

--- mad_memes_ingest.rb
+++ mad_memes_ingest.rb
@@ -96,7 +96,7 @@ end
 thresholds = thresholds.map {|thresh| thresh * n}.reverse
 
 # actually just hardcode rarities
-thresholds = [19.1, 61.1, 131.1, 388.1]
+thresholds = [19.1, 61.1, 130.1, 386.1]
 
 def categorize_rarity(rarity, thresholds)
   case
